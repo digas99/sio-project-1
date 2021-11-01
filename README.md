26759e8ec3c3b00bd0a36761b54cfae0182685e4
Diogo <diogo.correia99@ua.pt>
2021-11-01 01:10:27 +0000

    added some logic to login and signup pages

--- README.md
+++ README.md
@@ -8,9 +8,9 @@
 
 88930 - João Tomás Borges Simões - jtsimoes@ua.pt
 
-90327 - Diogo Costa Correia - diogo.correia99@ua.pt
-
 88964 - Afonso Domingos Cardoso - afonsocardoso@ua.pt
+
+90327 - Diogo Costa Correia - diogo.correia99@ua.pt
 
 ### Vulnerabilities
 - CWE-79: Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')
--- app/login.php
+++ app/login.php
@@ -1,3 +1,25 @@
+<?php
+	// check if there was a login submition
+	if (isset($_POST['login-submit'])) {
+		// require database handler page
+		require 'db-handler.php';
+
+		// fetch information from the login form
+		$username = $_POST['username'];
+		$pwd = $_POST['pwd'];
+
+		// empty fields handler
+		if (empty($username) || empty($pwd)) {
+			header("Location: login.php?error=emptyfields&username=".$username);
+			exit();
+		}
+
+		// TODO: check if user exists in the database, and if so, if the password is correct
+
+		// TODO: session_start() and header location dashboard.php
+	}
+?>
+
 <!doctype html>
 
 <html lang="en">
@@ -10,14 +32,28 @@
 
 <body style="text-align:center">
 	<div class="centered-container">
-		<h1>Login</h1>
+		<h1><a href="login.php">Login</a></h1>
+		<?php
+			// put error messages
+			if (isset($_GET['error'])) {
+				switch($_GET['error']) {
+					case "emptyfields":
+						echo '<p class="error-message">Fill in all fields!</p>';
+						break;
+				}
+			}
+		?>
 		<div>
 			<form action="login.php" method="post">
 				<input type="text" name="username" placeholder="Username...">
 				<input type="password" name="pwd" placeholder="Password...">
 				<button type="submit" name="login-submit">Login</button>
+				<?php
+					// fill in username from url query
+					if (isset($_GET['username'])) echo "<script>Array.from(document.getElementsByTagName('INPUT')).filter(tag => tag.name === 'username')[0].value = '".$_GET['username']."'; </script>";
+				?>
 			</form>
-			<div style="margin-top:10px">Don't have an account? <a href="signup.php">Signup</a><div>
+			<div style="margin-top:10px">Don't have an account? <a href="signup.php">Signup</a></div>
 		</div>
 	</div>
 
