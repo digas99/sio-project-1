4a7135155c196c9a93876a1603013d1e07b4beb6
João Tomás <jtsimoes@ua.pt>
2021-11-02 23:21:59 +0000

    Updated "Font Awesome" CDN to the last version Fixed main icon on top left corner not showing

--- app/signup.php
+++ app/signup.php
@@ -41,7 +41,7 @@
     <title>Criar conta | Área de Administração</title>
 
     <!-- Custom fonts for this template-->
-    <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet" type="text/css">
+    <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
