15660ae1623cfb6ba99ebc6209728688fee609f0
João Tomás <jtsimoes@ua.pt>
2021-11-07 12:21:32 +0000

    Improved change password page (issue #5) Fixed indentation

--- app/change-password.php
+++ app/change-password.php
@@ -59,27 +59,22 @@
                         <div class="col-xl-8 col-lg-7">
                             <div class="card shadow mb-4">
                                 <!-- Card Header - Dropdown -->
-                                <div
-                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Results</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Alteração da palavra-passe</h6>
                                 </div>
                                 <!-- Card Body -->
                                 <div class="card-body">
-                
+                                    <form method="post">
+                                        <div class="form-group">
+                                            <label for="new-password">Nova palavra-passe:</label>
+                                            <input type="password" name="new-password" class="form-control" placeholder="Escrever a nova palavra-passe" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="repeat-new-password">Confirmar nova palavra-passe:</label>
+                                            <input type="password" name="repeat-new-password" class="form-control" placeholder="Repetir a nova palavra-passe" required>
+                                        </div>
+                                        <button type="submit" name="change-submit" value="post" class="btn btn-primary">Alterar</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
