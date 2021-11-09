88eb75961186ee0e1c3dffd68c9b2550a966a098
Diogo <diogo.correia99@ua.pt>
2021-11-09 16:41:41 +0000

    added missing files of last commit

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sudo docker build -t webapp .
+# setup permissions for webapp files
+sudo chmod -R a+rwx ${PWD}/app
+sudo chmod -R a+rwx ${PWD}/app_sec
 
+# setup docker container
+sudo docker build -t webapp .
 sudo docker run -dti --name app -p 80:80 webapp
