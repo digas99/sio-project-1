14f2073e79fd876698d1cc5e975163e645a9ba9a
João Tomás <jtsimoes@ua.pt>
2021-11-09 23:18:14 +0000

    Fixed some last remaining translations

--- app/navbar.php
+++ app/navbar.php
@@ -68,15 +68,15 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Terminar sessão?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Clicar em "Terminar sessão" vai encerrar a sessão atual e será necessário voltar a fazer login.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.php?success=logout">Logout</a>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                    <a class="btn btn-primary" href="login.php?submit=logout">Terminar sessão</a>
                 </div>
             </div>
         </div>
