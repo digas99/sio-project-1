334b1eb37315663e76c6f28946cc62b90ae40fa6
João Tomás <jtsimoes@ua.pt>
2021-11-09 23:35:35 +0000

    Improved security of view logs page Added funny joke on both logs

--- app/php/log.php
+++ app/php/log.php
@@ -1,5 +1,5 @@
 [2021-11-05 15:50:50+00:00] : João Simões iniciou sessão através do IP 74.125.176.151 em Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 com o ID de sessão 1f3870be274f6c49b3e31a0c6728957f;
 [2021-11-05 15:52:10+00:00] : Diogo Correia iniciou sessão através do IP 142.209.162.59 em Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1 com o ID de sessão c18c9c57cb3658a50de06491a70b75cd;
-[2021-11-05 15:52:20+00:00] : João Simões publicou uma notícia com o ID 1 e com o título "Teste";
+[2021-11-05 15:52:20+00:00] : João Simões publicou uma notícia com o ID 1 e com o título "SIO é a melhor cadeira do mundo";
 [2021-11-05 15:59:10+00:00] : João Simões terminou sessão com o ID de sessão 1f3870be274f6c49b3e31a0c6728957f;
 [2021-11-07 20:20:20+00:00] : Carlos Costa iniciou sessão através do IP 95.251.103.100 em Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405 com o ID de sessão 0aed5d740d7fab4201e885019a36eace;
--- app_sec/php/log.php
+++ app_sec/php/log.php
@@ -1,5 +1,8 @@
+<?php
+    require '../../php/check-session.php';
+?>
 [2021-11-05 15:50:50+00:00] : João Simões iniciou sessão;
 [2021-11-05 15:52:10+00:00] : Diogo Correia iniciou sessão;
-[2021-11-05 15:52:20+00:00] : João Simões publicou uma notícia com o título "Teste";
+[2021-11-05 15:52:20+00:00] : João Simões publicou uma notícia com o título "SIO é a melhor cadeira do mundo";
 [2021-11-05 15:59:10+00:00] : João Simões terminou sessão;
 [2021-11-07 20:20:20+00:00] : Carlos Costa iniciou sessão;
