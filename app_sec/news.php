65de7b4a2ec1875aa421afcaaecbf34f80a93513
João Tomás <jtsimoes@ua.pt>
2021-11-10 00:39:18 +0000

    Improved security of view/delete news page

---
+++ app_sec/news.php
@@ -0,0 +1,199 @@
+<?php
+    require '../php/check-session.php';
+
+    // check if there was a delete submition
+	if(isset($_POST['delete-submit'])) {
+		// require database handler page
+		require '../php/db-handler.php';
+
+        // Make sure that value of ID is a integer and clean all other strings
+        $id = intval($_POST['delete-submit'], 10);
+        if (!is_int($id)){
+            header("Location: news.php?submit=invalid");
+            exit();
+        }
+        
+        // Delete news and reset auto increment to not leave blank IDs unused
+        $sql = "DELETE FROM news WHERE id=".$id."; ALTER TABLE news AUTO_INCREMENT = 1";
+        $query = mysqli_multi_query($conn, $sql);
+        if(mysqli_affected_rows($conn) === -1){
+            header("Location: news.php?submit=error");
+            exit();
+        }elseif(mysqli_affected_rows($conn) === 0){
+            header("Location: news.php?submit=invalid");
+            exit();
+        }else{
+            header("Location: news.php?submit=success");
+            exit();
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Gerir notícias | Área de Administração</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/4.1.4/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php
+            require 'sidebar.php';
+        ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Navbar -->
+                <?php
+                    require 'navbar.php';
+                ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Gerir Notícias</h1>
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <!-- Area Chart -->
+                        <div class="col-xl-8 col-lg-7">
+
+                            <?php
+                                // put error messages
+                                if (isset($_GET['submit'])) {
+                                    switch ($_GET['submit']) {
+                                        case 'success':
+                                            echo "
+                                                <div class=\"alert alert-success alert-dismissible fade show\">
+                                                    <i class=\"fas fa-check-circle\"></i> <strong>SUCESSO:</strong> A notícia foi eliminada com sucesso!
+                                                    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+                                                        <span aria-hidden=\"true\">×</span>
+                                                    </button>
+                                                </div>
+                                            ";
+                                            break;
+
+                                        case 'invalid':
+                                            echo "
+                                                <div class=\"alert alert-danger alert-dismissible fade show\">
+                                                    <i class=\"fas fa-times-circle\"></i> <strong>ERRO:</strong> Não foi encontrada nenhuma notícia com o ID selecionado!
+                                                    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+                                                        <span aria-hidden=\"true\">×</span>
+                                                    </button>
+                                                </div>
+                                            ";
+                                            break;
+
+                                        case 'error':
+                                            echo "
+                                                <div class=\"alert alert-danger alert-dismissible fade show\">
+                                                    <i class=\"fas fa-times-circle\"></i> <strong>ERRO:</strong> Ocorreu um problema ao tentar eliminar a notícia!
+                                                    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+                                                        <span aria-hidden=\"true\">×</span>
+                                                    </button>
+                                                </div>
+                                            ";
+                                            break;
+                                    }
+                                }
+                            ?>
+
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Lista de notícias</h6>
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <form method="post" onsubmit="return confirm('Tem a certeza que pretende eliminar permanentemente esta notícia?\nTenha em atenção que esta ação é irreversível.');">
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Título</th>
+                                                        <th>Imagem de Capa</th>
+                                                        <th>Corpo</th>
+                                                        <th>Autor</th>
+                                                        <th>Ações</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php
+                                                        // require database handler page
+		                                                require '../php/db-handler.php';
+                                                        
+                                                        $sql = "SELECT * FROM news";
+                                                        $result = mysqli_query($conn, $sql);
+
+                                                        while($row = mysqli_fetch_array($result)){
+                                                            echo "<tr>
+                                                                <td>" . $row['title'] . "</td>
+                                                                <td>
+                                                                    <img src=". $row['img'] . " alt=\"\" width=\"150\" class=\"img-fluid\">
+                                                                </td>
+                                                                <td>" . $row['body'] . "</td>
+                                                                <td>" . $row['author'] . "</td>
+                                                                <td>
+                                                                    <button title=\"Eliminar notícia " . $row['id'] . "\" type=\"submit\" class=\"btn btn-danger btn-block\" name=\"delete-submit\" value=\"" . $row['id'] . "\">
+                                                                        <i class=\"fas fa-trash\"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>";
+                                                        }
+                                                    ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/4.1.4/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
